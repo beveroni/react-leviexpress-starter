68c7b03ac0b3f56b4dc837c234c041e5468b4b5f
Veronika Bendova <svechovaver@gmail.com>
2023-05-27 10:46:10 +0200

    Dokončení části Údaje o cestě

--- src/components/JourneyPicker/index.jsx
+++ src/components/JourneyPicker/index.jsx
@@ -1,51 +1,67 @@
 import React, { useEffect, useState } from 'react';
 import './style.css';
 
-export const JourneyPicker = ({ onJourneyChange }) => (
-  <div className="journey-picker container">
-    <h2 className="journey-picker__head">Kam chcete jet?</h2>
-    <div className="journey-picker__body">
-      <form className="journey-picker__form">
-        <label>
-          <div className="journey-picker__label">Odkud:</div>
-          <select>
-            <option value="">Vyberte</option>
-            <option value="mesto01">Město 01</option>
-            <option value="mesto02">Město 02</option>
-            <option value="mesto03">Město 03</option>
-            <option value="mesto04">Město 04</option>
-            <option value="mesto05">Město 05</option>
-          </select>
-        </label>
-        <label>
-          <div className="journey-picker__label">Kam:</div>
-          <select>
-            <option value="">Vyberte</option>
-            <option value="mesto01">Město 01</option>
-            <option value="mesto02">Město 02</option>
-            <option value="mesto03">Město 03</option>
-            <option value="mesto04">Město 04</option>
-            <option value="mesto05">Město 05</option>
-          </select>
-        </label>
-        <label>
-          <div className="journey-picker__label">Datum:</div>
-          <select>
-            <option value="">Vyberte</option>
-            <option value="datum01">Datum 01</option>
-            <option value="datum02">Datum 02</option>
-            <option value="datum03">Datum 03</option>
-            <option value="datum04">Datum 04</option>
-            <option value="datum05">Datum 05</option>
-          </select>
-        </label>
-        <div className="journey-picker__controls">
-          <button className="btn" type="submit">
-            Vyhledat spoj
-          </button>
-        </div>
-      </form>
-      <img className="journey-picker__map" src="/map.svg" />
+export const JourneyPicker = ({ onJourneyChange }) => {
+  const [fromCity, setFromCity] = useState('mesto01');
+  const [toCity, setToCity] = useState('mesto01');
+  const [date, setDate] = useState('datum01');
+  const handleSubmit = (event) => {
+    event.preventDefault(); // Zabrání odeslání formuláře prohlížečem
+    console.log('Odesílám formulář s cestou');
+    console.log('Odkud:', fromCity);
+    console.log('Kam:', toCity);
+    console.log('Datum:', date);
+  };
+  return (
+    <div className="journey-picker container">
+      <h2 className="journey-picker__head">Kam chcete jet?</h2>
+      <div className="journey-picker__body">
+        <form className="journey-picker__form" onSubmit={handleSubmit}>
+          <label>
+            <div className="journey-picker__label">Odkud:</div>
+            <select
+              value={fromCity}
+              onChange={(e) => setFromCity(e.target.value)}
+            >
+              <option value="">Vyberte</option>
+              <option value="mesto01">Město 01</option>
+              <option value="mesto02">Město 02</option>
+              <option value="mesto03">Město 03</option>
+              <option value="mesto04">Město 04</option>
+              <option value="mesto05">Město 05</option>
+            </select>
+          </label>
+          <label>
+            <div className="journey-picker__label">Kam:</div>
+            <select value={toCity} onChange={(e) => setToCity(e.target.value)}>
+              <option value="">Vyberte</option>
+              <option value="mesto01">Město 01</option>
+              <option value="mesto02">Město 02</option>
+              <option value="mesto03">Město 03</option>
+              <option value="mesto04">Město 04</option>
+              <option value="mesto05">Město 05</option>
+            </select>
+          </label>
+          <label>
+            <div className="journey-picker__label">Datum:</div>
+            <select value={date} onChange={(e) => setDate(e.target.value)}>
+              {' '}
+              <option value="">Vyberte</option>
+              <option value="datum01">Datum 01</option>
+              <option value="datum02">Datum 02</option>
+              <option value="datum03">Datum 03</option>
+              <option value="datum04">Datum 04</option>
+              <option value="datum05">Datum 05</option>
+            </select>
+          </label>
+          <div className="journey-picker__controls">
+            <button className="btn" type="submit">
+              Vyhledat spoj
+            </button>
+          </div>
+        </form>
+        <img className="journey-picker__map" src="/map.svg" />
+      </div>
     </div>
-  </div>
-);
+  );
+};
